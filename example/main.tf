44987e032a431651cfffe976d09a4d3f02350ea9
David Kerins <dkerins@bitbox.ca>
2026-05-28 11:58:47 -0700

    add name_prefix and billing group tag

--- example/main.tf
+++ example/main.tf
@@ -15,11 +15,19 @@ data "aws_subnets" "default" {
 # }
 
 module "test_instance" {
+  source                 = "github.com/david-kerins/terraform-aws-in-depth//modules/ec2_instance"
+  instance_count         = 5
+  subnet_id              = data.aws_subnets.default.ids[0]
+  instance_type          = "t3.micro"
+  name_prefix            = "ddk"
+  enable_systems_manager = true
   #subnet_id    = var.subnet_id
-  subnet_id     = data.aws_subnets.default.ids[0]
-  source        = "github.com/david-kerins/terraform-aws-in-depth//modules/ec2_instance"
-  instance_type = "t3.micro"
+
+  tags = {
+    "BillingGroup" = "mygroup"
+  }
 }
+
 output "aws_instance_arn" {
   value = module.test_instance.aws_instance_arn
 }
