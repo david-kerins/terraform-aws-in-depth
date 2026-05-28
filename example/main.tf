a9d384e339ca05f01556104fed8c643003fa7752
David Kerins <dkerins@bitbox.ca>
2026-05-28 10:24:41 -0700

    make subnet_id use data resource and add output

--- example/main.tf
+++ example/main.tf
@@ -9,11 +9,20 @@ data "aws_subnets" "default" {
   }
 }
 
+# variable "subnet_id" {
+#   type        = string
+#   description = "The Subnet to launch this resource in."
+# }
+
 module "test_instance" {
+  #subnet_id    = var.subnet_id
+  subnet_id     = data.aws_subnets.default.ids[0]
   source        = "github.com/david-kerins/terraform-aws-in-depth//modules/ec2_instance"
-  subnet_id     = var.subnet_id
   instance_type = "t3.micro"
 }
 output "aws_instance_arn" {
   value = module.test_instance.aws_instance_arn
+}
+output "aws_subnet_id" {
+  value = data.aws_subnets.default.ids[0]
 }
